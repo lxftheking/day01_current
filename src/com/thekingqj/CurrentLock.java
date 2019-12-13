821203860c0d6044bded36d46dc280da1cdc9938
lxftheking <1806665962@qq.com>
2019-12-13 20:47:15 +0800

    尝试提交代码

--- src/com/thekingqj/CurrentLock.java
+++ src/com/thekingqj/CurrentLock.java
@@ -6,17 +6,19 @@ import java.util.concurrent.locks.ReentrantLock;
 
 //锁    操作    资源
 class Tickect {
-    private int m=30;
-    private  Lock lock=new ReentrantLock();
-    public void buy(){
+    private int m = 30;
+    private Lock lock = new ReentrantLock();
+
+    public void buy() {
         lock.lock();
-                try {
-                    if (m >0) {
-                        System.out.println(Thread.currentThread().getName()+"第："+(m--)+"张票被卖出");
-                    }
-                } finally {
-                 lock.unlock();
-                }
+        try {
+            if (m > 0) {
+                System.out.println("hhhhh");
+                System.out.println(Thread.currentThread().getName() + "第：" + (m--) + "张票被卖出");
+            }
+        } finally {
+            lock.unlock();
+        }
 
     }
 }
@@ -24,6 +26,8 @@ class Tickect {
 
 public class CurrentLock {
     public static void main(String[] args) {
+
+
         Tickect tickect = new Tickect();
         new Thread(()->{ for (int i = 0; i <30 ; i++) tickect.buy(); },"A").start();
         new Thread(()->{ for (int i = 0; i <30 ; i++) tickect.buy(); },"B").start();
