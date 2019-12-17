7e066e4f742da48ff534c076389fe6d02502b0a5
lxftheking <1806665962@qq.com>
2019-12-17 18:21:09 +0800

    修改+1

--- src/com/thekingqj/Tickcet.java
+++ src/com/thekingqj/Tickcet.java
@@ -14,7 +14,7 @@ class Tickect1 {
         try {
             if (m > 0) {
                 System.out.println(Thread.currentThread().getName() + "第：" + (m--) + "张票被卖出啦");
-                System.out.println("HHHHH");
+                System.out.println("HHhh");
             }
         } finally {
             lock.unlock();
