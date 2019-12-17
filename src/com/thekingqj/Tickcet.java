8e60ebb2eb70bc97e78a08e287c76b6b25a4e4f4
lxftheking <1806665962@qq.com>
2019-12-17 11:34:05 +0800

    卖票，用线程池

---
+++ src/com/thekingqj/Tickcet.java
@@ -0,0 +1,41 @@
+package com.thekingqj;
+
+
+import java.util.concurrent.*;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+class Tickect1 {
+    private int m = 300;
+    private Lock lock = new ReentrantLock();
+
+    public void buy() {
+        lock.lock();
+        try {
+            if (m > 0) {
+                System.out.println(Thread.currentThread().getName() + "第：" + (m--) + "张票被卖出");
+            }
+        } finally {
+            lock.unlock();
+        }
+
+    }
+}
+public class Tickcet {
+
+    public static void main(String[] args) {
+        ExecutorService threadpool= Executors.newFixedThreadPool(3);
+        Tickect1 t1=new Tickect1();
+        try {
+            for (int i = 1; i <=300 ; i++) {
+
+               threadpool.execute(()->{
+                           t1.buy();
+                       });
+            }
+        } finally {
+            threadpool.shutdown();
+
+        }
+    }
+}
