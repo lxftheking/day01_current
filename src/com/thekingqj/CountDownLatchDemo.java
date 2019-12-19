10090b0b23c09d38e0030ea640ec95e761514400
lxftheking <1806665962@qq.com>
2019-12-19 17:27:58 +0800

    修改+3

--- src/com/thekingqj/CountDownLatchDemo.java
+++ src/com/thekingqj/CountDownLatchDemo.java
@@ -6,6 +6,7 @@ import java.util.concurrent.CountDownLatch;
  * CountDownLatch主要有两个方法，当一个或多个线程调用await方法时，这些线程会阻塞。
  *   其它线程调用countDown方法会将计数器减1(调用countDown方法的线程不会阻塞)，
  *   当计数器的值变为0时，因await方法阻塞的线程会被唤醒，继续执行。
+ *   题目：6个学生+班长在教室上自习，只有班长有钥匙锁门，班长只有等六个学生都走完才能锁门
  */
 public class CountDownLatchDemo {
 
